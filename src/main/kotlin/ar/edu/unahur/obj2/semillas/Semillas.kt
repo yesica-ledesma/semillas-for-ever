156e02275ed386439006ba768a6db3a69a5f6eb1
yesica <84295985+yesica-ledesma@users.noreply.github.com>
2022-04-18 21:03:54 -0300

    Update Semillas.kt

--- src/main/kotlin/ar/edu/unahur/obj2/semillas/Semillas.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillas/Semillas.kt
@@ -58,3 +58,27 @@ class Peperina(altura: Double, anioSemilla: Int): Menta(altura , anioSemilla) {
 class SojaTransgénica(altura: Double, anioSemilla: Int): Soja(altura , anioSemilla) {
     override fun daSemillas()= false
 }
+class Parcela(var ancho:Int, var largo:Int, var horasDeSolQueRecibe:Int) {
+
+  var plantasEnParcela= mutableListOf<Planta>()
+
+  var superficieDeParcelas = ancho*largo
+
+  fun cantidadDePlantasQueTolera() =
+    if (ancho > largo){ superficieDeParcelas/5 }
+    else{(superficieDeParcelas/3)+largo}
+
+  //Sí si alguna de sus plantas tolera menos sol del que recibe la parcela;
+  fun tieneComplicaciones()=
+    plantasEnParcela.any{ it.horasDeSolToleradas <= horasDeSolQueRecibe }
+
+  fun plantarLa_(planta:Planta) {
+    if(cantidadDePlantasQueTolera() > plantasEnParcela.size && planta.horasDeSolToleradas+2 >= horasDeSolQueRecibe ) {
+      plantasEnParcela.add(planta)
+    }
+    else{
+      println("Error no se puede agregar Planta, por no haber espacio o no cumple con las especificaciones")
+    }
+
+  }
+
