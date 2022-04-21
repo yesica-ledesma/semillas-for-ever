0413e9f4c4b275b8835b40001851b14ddadb98b5
yesica <yesica.ledesma@estudiantes.edu.ar>
2022-04-20 21:49:25 -0300

    INTA

--- src/main/kotlin/ar/edu/unahur/obj2/inta/INTA.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/inta/INTA.kt
@@ -1,9 +1,14 @@
 package ar.edu.unahur.obj2.semillas
 
+
 object Inta {
     val parcelas = mutableListOf<Parcelas>()
 
     fun promedio() {
-        
+        val cantidadPlantas = parcelas.sumOf {it.plantas.size}
+        val cantidadParcelas = parcelas.size
+
+        return (cantidadParcelas == 0) {0} else {cantidadPlantas/cantidadParcelas}
+
     }
 }
